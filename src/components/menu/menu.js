2443d2a90b0442d49d5cdf06cacf83d1316ac9e2
Yuri Delgado <yuricd@bol.com.br>
2021-02-14 13:09:18 -0300

    Tweaks

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 
-import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { AnchorLink } from 'gatsby-plugin-anchor-links'
 
-import styles from './menu.module.scss';
+import styles from './menu.module.scss'
 
 const Menu = () => {
-
-  const [openedMenu, setOpenedMenu] = useState(false);
+  const [openedMenu, setOpenedMenu] = useState(false)
+  const [activeLink, setActiveLink] = useState('home')
 
   return (
     <nav className={`${styles.menu} ${openedMenu ? styles.opened : ''}`}>
       <div className={styles.icon}>
-        <i 
+        <i
           aria-label="Toggle menu"
           role="button"
           onKeyUp={handleClick}
-          onClick={handleClick} 
+          onClick={handleClick}
           className={`fas ${openedMenu ? 'fa-times' : 'fa-bars'}`}
           tabIndex={0}
         />
@@ -25,35 +25,52 @@ const Menu = () => {
       <ul>
         <li>
           <AnchorLink
+            {...getActiveClass('home')}
             to="/#home"
-            onAnchorLinkClick={handleClick}
-          >Home</AnchorLink>
+            onAnchorLinkClick={() => handleClick('home')}
+          >
+            Home
+          </AnchorLink>
         </li>
         <li>
           <AnchorLink
+            {...getActiveClass('projects')}
             to="/#projects"
-            onAnchorLinkClick={handleClick}
-          >Projects</AnchorLink>
+            onAnchorLinkClick={() => handleClick('projects')}
+          >
+            Projects
+          </AnchorLink>
         </li>
         <li>
           <AnchorLink
+            {...getActiveClass('about')}
             to="/#about"
-            onAnchorLinkClick={handleClick}
-          >A little about me</AnchorLink>
+            onAnchorLinkClick={() => handleClick('about')}
+          >
+            A little about me
+          </AnchorLink>
         </li>
         <li>
           <AnchorLink
+            {...getActiveClass('contact')}
             to="/#contact"
-            onAnchorLinkClick={handleClick}
-          >Let's talk</AnchorLink>
+            onAnchorLinkClick={() => handleClick('contact')}
+          >
+            Let's talk
+          </AnchorLink>
         </li>
       </ul>
     </nav>
-  );
+  )
 
-  function handleClick() {
-    setOpenedMenu(!openedMenu);
+  function handleClick(link) {
+    setOpenedMenu(!openedMenu)
+    setActiveLink(link)
+  }
+
+  function getActiveClass(current) {
+    return current === activeLink ? { className: styles.active } : ''
   }
 }
 
-export default Menu;
+export default Menu
