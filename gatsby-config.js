5c0eba01c758178be9e2b625c02399aac74db3dd
Yuri Delgado <yuricd@bol.com.br>
2021-02-24 21:14:02 -0300

    Change GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "263446378",
+        trackingId: "G-TB4Y46MFX9",
       },
     },
   ],
