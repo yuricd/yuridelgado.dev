2ae151ace686e1c5c2838d42fa04fddf1678b752
Yuri Delgado <yuricd@bol.com.br>
2021-02-24 22:18:05 -0300

    Change GTAG configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,15 +27,12 @@ module.exports = {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
+          send_page_view: true,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
       },
     },
