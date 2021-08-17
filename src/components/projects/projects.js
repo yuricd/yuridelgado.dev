f884c11e9722a7d91fcdd74b99fcf6c228c26eb4
Yuri Delgado <yuridelg@gmail.com>
2021-08-17 20:29:05 -0300

    Add recent projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import Title from '../title/title';
-import camara2019 from '../../assets/images/camara2019.png';
-import cfb from '../../assets/images/custom-fb-extension.png';
-import loa from '../../assets/images/lei-ou-nao.png';
-import poa from '../../assets/images/preso-ou-nao.png';
-import snake from '../../assets/images/snake.png';
-import svgFilter from '../../assets/images/svg-filter-effects.png';
+import Title from '../title/title'
+import camara2019 from '../../assets/images/camara2019.png'
+import cfb from '../../assets/images/custom-fb-extension.png'
+import loa from '../../assets/images/lei-ou-nao.png'
+import poa from '../../assets/images/preso-ou-nao.png'
+import snake from '../../assets/images/snake.png'
+import gimpThumbs from '../../assets/images/gimp-thumbs.png'
+import carbonFootprint from '../../assets/images/carbon-footprint-calculator.png'
+import svgFilter from '../../assets/images/svg-filter-effects.png'
 
-import styles from './projects.module.scss';
+import styles from './projects.module.scss'
 
 const Card = ({ image, title, subtitle, url }) => {
   return (
@@ -16,12 +18,15 @@ const Card = ({ image, title, subtitle, url }) => {
       <a href={url || '#'} target="_blank" rel="noopener noreferrer">
         <img src={image} alt={subtitle} />
         <figcaption>
-          <h3>{title}<i className="fas fa-external-link-alt" /></h3>
-          <p>{ subtitle }</p>
+          <h3>
+            {title}
+            <i className="fas fa-external-link-alt" />
+          </h3>
+          <p>{subtitle}</p>
         </figcaption>
       </a>
     </figure>
-  );
+  )
 }
 
 const Projects = () => {
@@ -32,7 +37,16 @@ const Projects = () => {
       <div className={styles.listProjects}>
         <div className={styles.left}>
           <div className={styles.item}>
-            <Card 
+            <Card
+              image={gimpThumbs}
+              title="BOT"
+              subtitle="Thumbnail Generator"
+              url="https://github.com/yuricd/gimp-scripting"
+            />
+          </div>
+
+          <div className={styles.item}>
+            <Card
               image="https://raw.githubusercontent.com/yuricd/scholiast/master/assets/scholiast-preview.png"
               title="Bot"
               subtitle="Scholiast - A comment bot for Instagram"
@@ -41,8 +55,8 @@ const Projects = () => {
           </div>
 
           <div className={styles.item}>
-            <Card 
-              image={snake} 
+            <Card
+              image={snake}
               title="Web App"
               subtitle="Snake Vai a Brasília"
               url="https://snake.yuridelgado.dev"
@@ -50,17 +64,17 @@ const Projects = () => {
           </div>
 
           <div className={styles.item}>
-            <Card 
-              image={loa} 
+            <Card
+              image={loa}
               title="Web App"
               subtitle="Lei ou Não"
               url="https://leiounao.herokuapp.com"
             />
           </div>
-         
+
           <div className={styles.item}>
-            <Card 
-              image={svgFilter} 
+            <Card
+              image={svgFilter}
               title="NPM Package"
               subtitle="SVG Filter Effect"
               url="https://github.com/yuricd/svg-filter-effects"
@@ -70,8 +84,16 @@ const Projects = () => {
 
         <div className={styles.right}>
           <div className={styles.item}>
-            <Card 
-              image={camara2019} 
+            <Card
+              image={carbonFootprint}
+              title="Web App"
+              subtitle="Personal Carbon Footprint Calculator"
+              url="https://github.com/yuricd/carbon-footprint"
+            />
+          </div>
+          <div className={styles.item}>
+            <Card
+              image={camara2019}
               title="Web App"
               subtitle="Câmara 2019"
               url="https://camara2019.yuridelgado.dev"
@@ -79,8 +101,8 @@ const Projects = () => {
           </div>
 
           <div className={styles.item}>
-            <Card 
-              image={poa} 
+            <Card
+              image={poa}
               title="Web App"
               subtitle="Preso ou Não?"
               url="https://presoounao.herokuapp.com"
@@ -88,18 +110,17 @@ const Projects = () => {
           </div>
 
           <div className={styles.item}>
-            <Card 
-              image={cfb} 
+            <Card
+              image={cfb}
               title="Chrome Extension"
               subtitle="Custom Facebook"
               url="https://github.com/yuricd/customize-social-network"
             />
           </div>
         </div>
-
       </div>
     </div>
   )
-};
+}
 
-export default Projects;
+export default Projects
